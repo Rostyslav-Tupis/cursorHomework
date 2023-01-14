a7342c7ec3acf20c68560f6586a93501f72bdf9f
Rostyslavichh <rostyslavichh@gmail.com>
2023-01-14 16:55:56 +0200

    Completed task 9

---
+++ HW JS 9/scripts/scripts.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded" , function(){
+
+    let wrapper = document.querySelector('.wrapper');
+    Object.assign(wrapper.style,{margin:"0 auto", maxWidth:"250px", display:"flex" , flexWrap:"wrap"})
+    
+    function getRandomColor(){
+        let randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
+        return randomColor
+    }
+
+    function generateBlocks() {
+        let div;
+        for (let i = 0 ; i < 25 ; i++){
+            div = document.createElement('div');
+            div.classList.add('block')
+            document.getElementById("wrap").append(div);
+            Object.assign(div.style,{width:"50px" , height:"50px" ,display:"inline-block" ,backgroundColor: getRandomColor()});
+        }
+    }
+    generateBlocks();
+    
+    // let button = document.createElement('button'); спроба зробити запуск generateBlocksInterval через кноку 
+    // Object.assign(button.style,{width:"300px" , height:"50px" ,display:"inline-block" ,backgroundColor: "aqua" , marginTop: "10px" , borderRadius:'15px'});
+    // button.innerText = "Start generate colors"
+    // document.getElementById("wrap").append(button);
+
+
+    function generateBlocksInterval(){
+        document.querySelectorAll('.block').forEach((elem) => {
+            elem.style.backgroundColor = getRandomColor();
+        })
+    }
+    setInterval(generateBlocksInterval , 1000)
+})
+
