e0c336f8b5d68bf48b1e75284c28784ad39440e5
Rostyslavichh <rostyslavichh@gmail.com>
2023-01-22 17:02:05 +0200

    completed task 11

---
+++ HW 11 JS /main.js
@@ -0,0 +1,13 @@
+async function getRandomChinese(length){
+    let sign = '';
+    for (let i = 0 ; i < length ; i++){
+        let date = Date.now().toString().slice(-5);
+        let symbol = String.fromCharCode(date);
+        sign += symbol;
+        await new Promise ((res)=>{ 
+            setTimeout(()=>{ res() },50)
+        })
+    }
+    console.log(sign);
+}
+getRandomChinese(4);
