4206d1c850f4512f6ac6b04f2e7d794edab1503f
Rostyslavichh <rostyslavichh@gmail.com>
2023-01-27 00:49:15 +0200

    completed task 12

--- HW JS 12/scripts/main.js
+++ HW JS 12/scripts/main.js
@@ -2,31 +2,45 @@ const submitBtn = document.querySelector('.serach_btns');
 const container = document.querySelector('.container')
 const characters = document.getElementById('characters');
 const btnback = document.querySelector('.back')
+const slider_planets = document.querySelector('.slider_planets')
 
-const planets = document.querySelector('.planets')
+//planets ! ----------------------------------------------------------------
+let planetsPrevious = document.getElementById('planets-previous')
+let planetsNext = document.getElementById('planets-next')
 
-//-----------------------------------------------------------------
+let URL_Planets = "https://swapi.dev/api/planets/?page=1";
+let nextPlanets;
+let previousPlanets;
+
+//--------------------------------------------------------------------------
+function removeInfo(info){
+    info.forEach(elem=>{
+        elem.remove(info)
+    })
+}
 async function getCharacterFromFlim(reviceCharacter){
+    let info = document.querySelectorAll('.info')
+    console.log(info);
+    removeInfo(info)
+    let error = document.querySelectorAll('.error_alert')
     const inputValue = document.querySelector('.inputFill').value;
     let mainLink = fetch(`https://swapi.dev/api/films/${inputValue}/`);
-
     try{
         let respones = await mainLink;
         let data = await respones.json();
-        container.classList.add('hide')
         let allCharacter =  data.characters;
+        error.forEach(elem=>{
+            elem.remove(info)
+        })
         reviceCharacter(allCharacter)
     }
     catch(err){
-        console.log(err);
         let alert = document.createElement("p")
         alert.classList.add("error_alert");
         alert.innerText = "Opps something goes wrong, please try again"
         characters.append(alert)
     }
-
 }
-let photoOfCharacter = {}
 function reviceCharacter(characters){
     characters.forEach(element => {
         fetch(element).then((respones)=>{
@@ -40,7 +54,7 @@ function reviceCharacter(characters){
 function creatDiv(data){
     characters.innerHTML += `
     <div class="info">
-        <img src='photos/${data.name}.webp'  alt="photo"/ class="image">
+        <img src='photos/${data.name}.webp'  alt="photo"" class="image">
         <div class="allInfo">
             <h2 class="name">${data.name}</h2>
             <p class="years">${data.birth_year}</p>
@@ -49,22 +63,45 @@ function creatDiv(data){
     </div>
     ` ;
 }
+
 //show planets-----------------------------------------------------------------
-async function getPlanets(){
-    let response = await fetch("https://swapi.dev/api/planets/");
-    let data = await response.json();
-    let planets = data.results;
-    getNameOfPlanet(planets)
-}
-function getNameOfPlanet(arr){
-    for(let obj of arr){
-        let name = obj.name
-        creatPlanetsDiv(name)
+async function getPlanets() {
+    let results = await fetch(URL_Planets);
+    const data = await results.json();
+
+    nextPlanets = data.next;          //зберігаємо посилання на наступну Лінку з планетами  
+    previousPlanets = data.previous;
+
+    let planets = data.results; //дістаємо масив з об'єктами про планети 
+    let outPut = ' ';
+    planets.forEach(planet => {
+      outPut += `<div class="card">
+                    <h2>${planet.name}</h2>
+                 </div>`
+    })
+    slider_planets.innerHTML = outPut;
+  }
+
+  function pageNextPlanets() {
+    if(nextPlanets) {
+      URL_Planets = new URL(nextPlanets); // передається лінк з функції вище 
     }
-}
-function creatPlanetsDiv(name){
-    planets.innerHTML += `
-    <p class="nameOfPlanet">${name}</p>
-    `
-}
-getPlanets()
+    getPlanets()
+      .then(response => { 
+    })
+      .catch(error => { 
+        console.log(error)
+    });
+  }
+  
+  function pagePreviousPlanets() {
+    if(previousPlanets) {
+      URL_Planets = new URL(previousPlanets);
+    }
+    getPlanets()
+      .then(response => { 
+    })
+      .catch(error => { 
+      console.log(error)
+    });
+  }
